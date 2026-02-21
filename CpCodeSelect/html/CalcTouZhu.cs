b3ad2e3791a613bd14c9592c0316e11ba9fcb8d9
guanyanyong <119549119+guanyanyong@users.noreply.github.com>
2026-02-22 01:11:09 +0800

    添加计算期数倍率的逻辑

---
+++ CpCodeSelect/html/CalcTouZhu.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CpCodeSelect.html
+{
+    public class CalcTouZhu
+    {
+        public CalcTouZhu()
+        {
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="initialPrincipal">初始本金</param>
+        /// <param name="CurrentPrincipal">当前本金</param>
+        public CalcTouZhu(decimal initialPrincipal, decimal currentPrincipal)
+        {
+            this.InitialPrincipal = initialPrincipal;
+            this.CurrentPrincipal = currentPrincipal;
+        }
+        public int ClickCount = 0;
+        public int SplitStage = 1;  // 当前拆分阶段 
+        public int BaseClicks = 5;  // 每阶段需要点击的次数
+        public int TotalZhong = 0;  // 中的次数
+        public int TotalGua = 0;    // 挂的次数
+        public int TotalTime = 0;   // 总的点击次数
+        /// <summary>
+        /// 初始本金，默认为200元，可以根据需要调整
+        /// </summary>
+        public decimal InitialPrincipal = 2000M;
+        /// <summary>
+        /// 当前余额，初始为200元，每次点击后根据盈亏情况更新
+        /// </summary>
+        public decimal CurrentPrincipal = 2000M;
+        public decimal CurrentBetAmount = 0M; // 当前投注倍数
+        public decimal MaxPrincipal = 210M; // 当前最大本金，初始为200元，每次超过5%后更新
+        public decimal CurrentSplitAmount = 0; // 当前拆分金额
+        public decimal CurrentProfitLoss = 0; // 当前盈亏
+        public decimal BetAccountBalance = 0; // 投注账户余额
+        public void TouZhu()
+        {
+            // 增加点击次数
+            ClickCount++;
+
+            // 更新拆分阶段和金额
+            if (SplitStage <= 10)
+            {
+                CurrentSplitAmount = (InitialPrincipal * SplitStage) / 200;
+                if (ClickCount % BaseClicks == 0) SplitStage++;
+            }
+            else if (SplitStage <= 30)
+            {
+                CurrentSplitAmount = (InitialPrincipal * SplitStage) / 200;
+                if (ClickCount % BaseClicks == 0) SplitStage += 2;
+            }
+            else if (SplitStage <= 60)
+            {
+                CurrentSplitAmount = (InitialPrincipal * SplitStage) / 200;
+                if (ClickCount % BaseClicks == 0) SplitStage += 3;
+            }
+            else
+            {
+                //todo 这里可以考虑重置或者其他处理方式，目前先提示无法继续拆分
+                MessageBox.Show("已达到最大拆分阶段，无法继续拆分。");
+                return;
+            }
+
+            // 计算当前盈亏
+            CurrentProfitLoss = CurrentPrincipal - InitialPrincipal;
+
+            // 计算投注账户余额
+            BetAccountBalance = CurrentProfitLoss / 8.0M + CurrentSplitAmount;
+
+            // 计算当期投注倍数
+            CurrentBetAmount = Math.Round(BetAccountBalance / 0.35M, 0);
+
+        }
+        /// <summary>
+        /// 返回是否是最大本金变化的情况
+        /// </summary>
+        /// <param name="isZhong">判断是否中奖</param>
+        /// <returns></returns>
+        public bool KaiJiang(bool isZhong)
+        {
+            var maxChange = false;
+            if (!isZhong)
+            {
+                CurrentPrincipal = CurrentPrincipal - 0.35M * CurrentBetAmount;
+                TotalGua++;
+            }
+            else
+            {
+                TotalZhong++;
+                CurrentPrincipal = CurrentPrincipal + 0.98M * CurrentBetAmount;
+                if (CurrentPrincipal >= MaxPrincipal)
+                {
+                    InitialPrincipal = CurrentPrincipal;
+                    MaxPrincipal = Math.Round(CurrentPrincipal * 1.05M);
+                    MaxChangeInit();
+                    maxChange = true;
+                }
+            }
+
+            TotalTime = TotalGua + TotalZhong;
+
+            decimal CurrentSplitAmount;
+            if (SplitStage <= 10)
+            {
+                CurrentSplitAmount = (InitialPrincipal * SplitStage) / 200;
+            }
+            else if (SplitStage <= 30)
+            {
+                CurrentSplitAmount = (InitialPrincipal * SplitStage) / 200;
+            }
+            else if (SplitStage <= 60)
+            {
+                CurrentSplitAmount = (InitialPrincipal * SplitStage) / 200;
+            }
+            else
+            {
+                MessageBox.Show("已达到最大拆分阶段，无法继续拆分。");
+                return maxChange;
+            }
+
+            // 计算当前盈亏
+            CurrentProfitLoss = CurrentPrincipal - InitialPrincipal;
+
+            // 计算投注账户余额
+            BetAccountBalance = CurrentProfitLoss / 8.0M + CurrentSplitAmount;
+
+            // 计算当期投注倍数
+            CurrentBetAmount = Math.Abs(Math.Round(BetAccountBalance / 0.35M, 0));
+            if (CurrentBetAmount == 0) CurrentBetAmount = 1;
+
+            return maxChange;
+        }
+
+        public void Reset()
+        {
+            InitialPrincipal = 2000M;
+            CurrentPrincipal = 2000M;
+            ClickCount = 0;
+            SplitStage = 1; // 当前拆分阶段 
+            BaseClicks = 5; // 每阶段需要点击的次数
+
+
+            MaxPrincipal = InitialPrincipal * 1.05M; // 重置最大本金为初始本金的105%
+
+            TotalZhong = 0;  // 中的次数
+            TotalGua = 0;   // 挂的次数
+            TotalTime = 0;   // 总的点击次数
+        }
+
+
+        private void MaxChangeInit()
+        {
+
+            ClickCount = 0;
+            SplitStage = 1; // 当前拆分阶段 
+            BaseClicks = 5; // 每阶段需要点击的次数
+
+
+        }
+    }
+}
--- CpCodeSelect/html/zhihuiwo.cs
+++ CpCodeSelect/html/zhihuiwo.cs
@@ -13,7 +13,7 @@ namespace CpCodeSelect.html
 {
     public partial class zhihuiwo : Form
     {
-        public int ClickCount = 0;
+        /*public int ClickCount = 0;
         public int SplitStage = 1;  // 当前拆分阶段 
         public int BaseClicks = 5;  // 每阶段需要点击的次数
         public int TotalZhong = 0;  // 中的次数
@@ -29,13 +29,50 @@ namespace CpCodeSelect.html
         public decimal CurrentPrincipal = 200M;
         public decimal currentBetAmount = 0M; // 当前投注倍数
         public decimal MaxPrincipal = 210M; // 当前最大本金，初始为200元，每次超过5%后更新
+        */
+        private CalcTouZhu CalcTouZhu;
         public zhihuiwo()
         {
+            CalcTouZhu = new CalcTouZhu(2000, 2000);
             InitializeComponent();
+            InitForm();
+        }
+        public void InitForm()
+        {
+            // 计算当期投注倍数
+            lblCurrentBetAmount.Text = CalcTouZhu.CurrentBetAmount.ToString();
+
+            lblClickCount.Text = "0";
+            lblSplitAmount.Text = "0";
+            lblSplitStage.Text = "1";
+            lblCurrentProfitLoss.Text = "0";
+            lblBetAccountBalance.Text = "0";
+
+            lblTotal.Text = "总计0个";
+            lblZhong.Text = "中0个";
+            lblGua.Text = "挂0个";
+            txtInitAmount.Text = CalcTouZhu.InitialPrincipal.ToString();
+                txtCurrentAmount.Text = CalcTouZhu.CurrentPrincipal.ToString();
         }
 
         private void btnCalc_Click(object sender, EventArgs e)
         {
+            CalcTouZhu.TouZhu();
+            lblClickCount.Text = CalcTouZhu.ClickCount.ToString();
+
+            lblSplitAmount.Text = CalcTouZhu.CurrentSplitAmount.ToString("F2");
+            lblSplitStage.Text = CalcTouZhu.SplitStage.ToString();
+
+            // 计算当前盈亏
+            var currentProfitLoss = CalcTouZhu.CurrentPrincipal - CalcTouZhu.InitialPrincipal;
+            lblCurrentProfitLoss.Text = CalcTouZhu.CurrentProfitLoss.ToString("F2");
+
+            // 计算投注账户余额
+            lblBetAccountBalance.Text = CalcTouZhu.BetAccountBalance.ToString("F2");
+
+            // 计算当期投注倍数
+            lblCurrentBetAmount.Text = CalcTouZhu.CurrentBetAmount.ToString();
+            /*
             // 增加点击次数
             ClickCount++;
             lblClickCount.Text = ClickCount.ToString();
@@ -76,45 +113,31 @@ namespace CpCodeSelect.html
             // 计算当期投注倍数
             currentBetAmount = Math.Round(betAccountBalance / 0.35M, 0);
             lblCurrentBetAmount.Text = currentBetAmount.ToString();
-
+            */
 
         }
 
         private void btnReset_Click(object sender, EventArgs e)
         {
-            InitialPrincipal = 200M;
-            CurrentPrincipal = 200M;
-            ClickCount = 0;
-            SplitStage = 1; // 当前拆分阶段 
-            BaseClicks = 5; // 每阶段需要点击的次数
-
+            CalcTouZhu.Reset();
             lblClickCount.Text = "0";
             lblSplitAmount.Text = "0";
             lblSplitStage.Text = "1";
             lblCurrentProfitLoss.Text = "0";
             lblBetAccountBalance.Text = "0";
-
-            // 计算当期投注倍数
             lblCurrentBetAmount.Text = "0";
 
-            txtInitAmount.Text = "200";
-            txtCurrentAmount.Text = "200";
-            MaxPrincipal = InitialPrincipal*1.05M; // 重置最大本金为初始本金的105%
-
-            TotalZhong = 0;  // 中的次数
-            TotalGua = 0;   // 挂的次数
-            TotalTime = 0;   // 总的点击次数
             lblTotal.Text = "总计0个";
             lblZhong.Text = "中0个";
             lblGua.Text = "挂0个";
+
+            txtInitAmount.Text = CalcTouZhu.InitialPrincipal.ToString();
+            txtCurrentAmount.Text = CalcTouZhu.CurrentPrincipal.ToString();
         }
 
         private void MaxChangeInit()
         {
 
-            ClickCount = 0;
-            SplitStage = 1; // 当前拆分阶段 
-            BaseClicks = 5; // 每阶段需要点击的次数
 
             lblClickCount.Text = "0";
             lblSplitAmount.Text = "0";
@@ -125,12 +148,37 @@ namespace CpCodeSelect.html
             // 计算当期投注倍数
             lblCurrentBetAmount.Text = "0";
 
-            txtInitAmount.Text = InitialPrincipal.ToString();
-            txtCurrentAmount.Text = CurrentPrincipal.ToString();
+            txtInitAmount.Text = CalcTouZhu.InitialPrincipal.ToString();
+            txtCurrentAmount.Text = CalcTouZhu.CurrentPrincipal.ToString();
         }
 
         private void btnResult_Click(object sender, EventArgs e)
         {
+            var isZhong = rbZhong.Checked;
+            if (CalcTouZhu.KaiJiang(isZhong))
+            {
+                MaxChangeInit();
+            }
+
+
+            txtCurrentAmount.Text = CalcTouZhu.CurrentPrincipal.ToString("F2");
+            lblGua.Text = string.Format($"挂{CalcTouZhu.TotalGua}");
+                lblZhong.Text = string.Format($"中{CalcTouZhu.TotalZhong}");
+                txtCurrentAmount.Text = CalcTouZhu.CurrentPrincipal.ToString("F2");
+
+            lblTotal.Text = string.Format($"总计{CalcTouZhu.TotalTime}");
+
+            lblSplitAmount.Text = CalcTouZhu.CurrentSplitAmount.ToString("F2");
+            lblSplitStage.Text = CalcTouZhu.SplitStage.ToString();
+
+            // 计算当前盈亏
+            lblCurrentProfitLoss.Text = CalcTouZhu.CurrentProfitLoss.ToString("F2");
+
+            // 计算投注账户余额
+            lblBetAccountBalance.Text = CalcTouZhu.BetAccountBalance.ToString("F2");
+
+            /*
+
             if (!rbZhong.Checked)
             {
                 CurrentPrincipal = CurrentPrincipal - 0.35M * currentBetAmount;
@@ -148,7 +196,6 @@ namespace CpCodeSelect.html
                 {
                     InitialPrincipal = CurrentPrincipal;
                     MaxPrincipal = Math.Round(CurrentPrincipal * 1.05M);
-                    MaxChangeInit();
                 }
             }
 
@@ -188,11 +235,12 @@ namespace CpCodeSelect.html
             currentBetAmount = Math.Abs(Math.Round(betAccountBalance / 0.35M, 0));
             if (currentBetAmount == 0) currentBetAmount = 1;
             lblCurrentBetAmount.Text = currentBetAmount.ToString();
+            */
         }
 
         private void txtInitAmount_Leave(object sender, EventArgs e)
         {
-            if (!decimal.TryParse(txtInitAmount.Text, out InitialPrincipal))
+            if (!decimal.TryParse(txtInitAmount.Text, out CalcTouZhu.InitialPrincipal))
             {
                 MessageBox.Show("请输入有效的初始本金金额。");
                 txtInitAmount.Focus();
@@ -201,7 +249,7 @@ namespace CpCodeSelect.html
 
         private void txtCurrentAmount_Leave(object sender, EventArgs e)
         {
-            if (!decimal.TryParse(txtCurrentAmount.Text, out CurrentPrincipal))
+            if (!decimal.TryParse(txtCurrentAmount.Text, out CalcTouZhu.CurrentPrincipal))
             {
                 MessageBox.Show("请输入有效的当前本金金额。");
                 txtCurrentAmount.Focus();
